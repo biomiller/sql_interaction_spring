5b334e13d6c4257ed18237e50b97b54d41db0adc
biomiller <35339306+biomiller@users.noreply.github.com>
2019-08-19 15:45:37 +0100

    Update SqlInteractionDemoApplication.java

--- sql_interaction_demo/src/main/java/com/bae/SqlInteractionDemoApplication.java
+++ sql_interaction_demo/src/main/java/com/bae/SqlInteractionDemoApplication.java
@@ -3,10 +3,7 @@ package com.bae;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
 @SpringBootApplication
-@EnableEncryptableProperties
 public class SqlInteractionDemoApplication {
 
 	public static void main(String[] args) {
